94e8a4de9b86b9438a5ee32bf5308a49314d33e9
TUMnatthapon <natthapon.tum01@gmail.com>
2020-04-09 20:12:42 +0700

    first commit

--- src/Component/Display.js
+++ src/Component/Display.js
@@ -5,7 +5,7 @@ import 'firebase/auth';
 const Display = props => {
     return (
         <div className='bearlist-container'>
-            <h1> Project</h1>
+            <h1> TestProject</h1>
             <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
             
         </div>
