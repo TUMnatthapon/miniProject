cf3538ecea5dddac452e33db9f0c4d2ddb79473f
TUMnatthapon <natthapon.tum01@gmail.com>
2020-04-14 22:02:20 +0700

    first commit

--- src/Component/Display.js
+++ src/Component/Display.js
@@ -5,7 +5,7 @@ import 'firebase/auth';
 const Display = props => {
     return (
         <div className="container">
-        <h1>FirebaseUI</h1>
+        <h1>FirebaseUI Test 1</h1>
         <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
           <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL}/>
         <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
